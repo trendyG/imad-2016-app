dc0acd9484616fc5ca7441a3c435279dc9773874
trendyG <giitamathai@gmail.com>
2016-09-29 15:21:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,10 @@ var element=document.getElementById("main-text"
 );
 
 element.innerHTML="New Value";
+
+//move the image
+
+var img=document.getElementById("madi");
+img.onClick=function(){
+    img.style.marginLeft = "100px";
+};
